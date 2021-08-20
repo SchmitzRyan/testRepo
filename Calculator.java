3db1d00b7f291e1961986f06b63cf1da901ac560
ryan schmitz <schmitzryan17@gmail.com>
2021-08-20 01:15:43 -0500

    initial test commit

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+//Class Calculator
+//XerexB
+
+import java.util.*;
+
+public class Calculator
+{
+    public static void main(String[] args)
+    {
+        prompt();
+    }
+
+    public static void prompt()
+    {
+        //define variables
+        //prompt user for input string
+        //parse input
+        //pass variables to appropriate helper method
+
+        System.out.println("---testing---");
+    }
+}
